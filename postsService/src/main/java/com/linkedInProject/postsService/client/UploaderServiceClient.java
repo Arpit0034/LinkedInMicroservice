3c01c1949ad386919d7317d9e5cafdd887cc3b52
Arpit0034 <kandariarpit096@gmail.com>
2026-04-07 00:38:41 +0530

    create api-gateway.yml + ingress.yml and fix application-k8s.properties + open-feign for uploader and post create to support multipart file

--- postsService/src/main/java/com/linkedInProject/postsService/client/UploaderServiceClient.java
+++ postsService/src/main/java/com/linkedInProject/postsService/client/UploaderServiceClient.java
@@ -1,13 +1,21 @@
 package com.linkedInProject.postsService.client;
 
+import com.linkedInProject.postsService.config.AppConfig;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "uploader-service" , path = "/uploads/file",url="${UPLOADER_SERVICE_URI:}")
+@FeignClient(
+        name = "uploader-service",
+        path = "/uploads/file",
+        url = "${UPLOADER_SERVICE_URI:}",
+        configuration = AppConfig.class
+)
 public interface UploaderServiceClient {
-    @PostMapping
-    public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file);
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file);
 }
--- postsService/src/main/java/com/linkedInProject/postsService/config/AppConfig.java
+++ postsService/src/main/java/com/linkedInProject/postsService/config/AppConfig.java
@@ -1,6 +1,10 @@
 package com.linkedInProject.postsService.config;
 
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +13,9 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper() ;
+    }
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(HttpMessageConverters::new));
     }
 }
